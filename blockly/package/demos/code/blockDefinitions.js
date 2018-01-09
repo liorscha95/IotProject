514c53e436cbea2ed3684a0d7678e9eab6bdd84c
liorscha <liorschachter10@gmail.com>
2018-01-10 01:13:25 +0200

    added all blocks and startes working on the generators, block.js is not needed anymore

---
+++ blockly/package/demos/code/blockDefinitions.js
@@ -0,0 +1,68 @@
+Blockly.Blocks['sethouse'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Set House behaviour");
+        this.appendDummyInput()
+            .appendField("for the following people: ");
+        this.appendStatementInput("PersonTasks")
+            .setCheck("PeopleBehaviour");
+        this.setColour(230);
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+
+Blockly.Blocks['manbehaviour'] = {
+  init: function() {
+    this.appendStatementInput("personCode")
+        .setCheck(["ShowerTask", "CoffeeTask"])
+        .appendField("When")
+        .appendField(new Blockly.FieldDropdown([["Michael","michael"], ["Daniel","Daniel"], ["Ruti","OPTIONNAME"]]), "person_name")
+        .appendField("Enters");
+    this.setPreviousStatement(true, "PeopleBehaviour");
+    this.setNextStatement(true, "PeopleBehaviour");
+    this.setColour(90);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['showertask'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Fill the tub with");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldNumber(0, 0, 10), "cycles")
+        .appendField("Cycles of water.");
+    this.appendDummyInput()
+        .appendField("In each cycle, ");
+    this.appendDummyInput()
+        .appendField("Pour hot water")
+        .appendField(new Blockly.FieldNumber(0, 0, 5), "hot quantity")
+        .appendField("times,");
+    this.appendDummyInput()
+        .appendField("And cold water")
+        .appendField(new Blockly.FieldNumber(0, 0, 5), "cold quantity")
+        .appendField("times");
+    this.setInputsInline(false);
+    this.setPreviousStatement(true, "ShowerTask");
+    this.setNextStatement(true, "CoffeeTask");
+    this.setColour(210);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
+
+Blockly.Blocks['coffeetask'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("Make")
+        .appendField(new Blockly.FieldDropdown([["Dark","dark"], ["Cappuccino","cappuccino"], ["Nescafe","nescafe"]]), "coffe type")
+        .appendField("Coffee");
+    this.setPreviousStatement(true, "CoffeeTask");
+    this.setColour(30);
+ this.setTooltip("");
+ this.setHelpUrl("");
+  }
+};
---
+++ blockly/package/demos/code/codeGenerators.js
@@ -0,0 +1,63 @@
+Blockly.JavaScript['sethouse'] = function(block) {
+    var statements_persontasks = Blockly.JavaScript.statementToCode(block, 'PersonTasks');
+    var code = 'var requestShowerES = bp.EventSet("shower requests", function (evt) {\n' +
+        '    return evt.name.indexOf("RequestShower") >= 0;\n' +
+        '});\n' +
+        '\n' +
+        'var requestCofeeES = bp.EventSet("coffee requests", function (evt) {\n' +
+        '    return evt.name.indexOf("requestCofee") >= 0;\n' +
+        '});\n' +
+        '\n' +
+        'var emptyShowerEvent = bp.Event("emptyShowerEvent");\n' +
+        'var endOfShowerEvent = bp.Event("endOfShowerEvent");\n' +
+        '\n' +
+        'var coldEvent = bp.Event("coldEvent");\n' +
+        'var finishColdEvent = bp.Event("finishColdEvent");\n' +
+        '\n' +
+        'var hotEvent = bp.Event("hotEvent");\n' +
+        'var finishHotEvent = bp.Event("finishHotEvent");\n' +
+        '\n' +
+        'var requestDarkCofeeEvent = bp.Event("requestCoffeeDarkEvent");\n' +
+        'var requestCapuccinoEvent = bp.Event("requestCoffeeCapuccinoEvent");\n' +
+        'var finishCofeeEvent = bp.Event("finishCofeeEvent");\n' +
+        '\n' +
+        '// Global functions and events\n' +
+        'bp.registerBThread("blockShower", function () {\n' +
+        '    while (true) {\n' +
+        '        bsync({waitFor: requestShowerES});\n' +
+        '        bsync({waitFor: emptyShowerEvent, block: requestShowerES});\n' +
+        '    }\n' +
+        '});\n' +
+        '\n' +
+        'bp.registerBThread("blockCoffee", function () {\n' +
+        '    while (true) {\n' +
+        '        bsync({waitFor: requestCofeeES});\n' +
+        '        bsync({waitFor: finishCofeeEvent, block: requestCofeeES});\n' +
+        '    }\n' +
+        '});\n';
+    return code + statements_persontasks;
+};
+
+Blockly.JavaScript['manbehaviour'] = function(block) {
+  var dropdown_person_name = block.getFieldValue('person_name');
+  var statements_personcode = Blockly.JavaScript.statementToCode(block, 'personCode');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['showertask'] = function(block) {
+  var number_cycles = block.getFieldValue('cycles');
+  var number_hot_quantity = block.getFieldValue('hot quantity');
+  var number_hot_quantity = block.getFieldValue('cold quantity');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['coffeetask'] = function(block) {
+  var dropdown_coffe_type = block.getFieldValue('coffe type');
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
