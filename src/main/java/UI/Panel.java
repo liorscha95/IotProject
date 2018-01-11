ac51128dfc35944c049a952a26a21fd7be0655a9
אופיר ממו <mamoo@post.bgu.ac.il>
2018-01-12 00:02:18 +0200

    images fix

--- src/main/java/UI/Panel.java
+++ src/main/java/UI/Panel.java
@@ -61,12 +61,12 @@ public class Panel {
 
 	private void buildCoffeeIcon() {
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\Ofir\\eclipse-workspace\\HomePanel\\bin\\coff.png"));
+		lblNewLabel.setIcon(new ImageIcon(getClass().getResource("/coffee.png")));
 		lblNewLabel.setBounds(585, 20, 228, 143);
 		frame.getContentPane().add(lblNewLabel);
 		
 		JLabel showerIcon = new JLabel("");
-		showerIcon.setIcon(new ImageIcon("C:\\Users\\Ofir\\eclipse-workspace\\HomePanel\\bin\\shower.png"));
+		showerIcon.setIcon(new ImageIcon(getClass().getResource("/shower.png")));
 		showerIcon.setBounds(300, 20, 228, 143);
 		frame.getContentPane().add(showerIcon);
 	}
